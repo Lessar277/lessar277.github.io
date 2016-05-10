9237c68b7024eb2a0514512afadf3321a91baba2
Lessar27 <lessar277@gmail.com>
2016-05-10 18:41:44 +0300

    changes to responsiveness

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 new WOW().init();
 
+$(window).resize(function () {
+	if ($('.toggle-nav').css('display') === 'none') {
+		$('.nav-list').css('display', 'flex');
+	} else {
+		$('.nav-list').css('display','none');
+	}
+})
+
 $('.toggle-nav, .nav a').click(function ()  {
     $('.nav-list').slideToggle();
 })
